a27dd340374371e5b748b6d117943fa61a1b9f80
Shruti <shrutisablaniya93@gmail.com>
2021-10-07 13:21:19 +0530

    Creating queue using linked list

---
+++ UC3_QueueCreation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructurePrograms
+{      
+    class UC3_QueueCreation
+    {
+            class Node
+            {
+                public int data;
+                public Node next;
+                public Node(int data)
+                {
+                    this.data = data;
+                    next = null;
+                }
+            }
+
+            Node head = null;
+            internal void Enqueue(int data)
+            {
+                Node node = new Node(data);
+                if (head == null)
+                    head = node;
+                else
+                {
+                    Node temp = head;
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                }
+                Console.WriteLine("{0} inserted into queue ", node.data);
+
+            }
+           
+            internal void Display()
+            {
+                Node temp = this.head;
+                if (temp == null)
+                {
+                    Console.WriteLine("Queue is empty");
+                    return;
+                }
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+       }
+}
